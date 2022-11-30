83a45ee28f6587b44d5f303b9c440d1b3c7210fd
Avinash Reddy <71509695+THEAVINASHREDDY@users.noreply.github.com>
2022-11-30 19:13:50 +0530

    Time: 6 ms (44.07%), Space: 39.2 MB (83.24%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        
+         if (n==0){
+            return 0;
+        }
+          else if(n<=2){
+            return 1;
+        }
+       
+        
+        return fib(n-1)+fib(n-2);
+    
+       
+      
+}
+}
